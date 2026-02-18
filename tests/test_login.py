5527465a7f686bc13de96d74087e061b1a9948cd
Fernando-Rubio <fernando.r.rubio99@gmail.com>
2026-02-17 19:53:26 -0800

    Implemented data-driven login test using pytest parametrize

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,7 +1,14 @@
+import pytest
 from pages.login_pages import LoginPage
 
-def test_valid_login(driver):
-    login = LoginPage(driver)
-    login.login("standard_user", "secret_sauce")
-    assert "inventory" in driver.current_url 
+@pytest.mark.parametrize("username,password,expected_result" , [("standard_user", "secret_sauce", "success"), ("locked_out_user", "secret_sauce", "locked"), ("problem_user", "secret_sauce", "success"), ("performance_glitch_user", "secret_sauce", "success"),])
+def test_login_scenarios(driver, username, password, expected_result):
+    login_page = LoginPage(driver)
+    login_page.login(username, password)
 
+    if expected_result == "success":
+        assert "inventory" in driver.current_url
+
+    elif expected_result == "locked":
+        error_message = login_page.get_error_message()
+        assert "locked out" in error_message.lower()

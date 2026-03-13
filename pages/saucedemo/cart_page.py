e53a63f9a4e65f4472db827069b4f164174d960b
Fernando-Rubio <fernando.r.rubio99@gmail.com>
2026-03-13 16:54:18 -0700

    Stabilize cart checkout wait for CI

--- pages/saucedemo/cart_page.py
+++ pages/saucedemo/cart_page.py
@@ -1,8 +1,10 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
 
 class CartPage(BasePage):
     CHECKOUT_BUTTON = (By.ID, "checkout")
 
     def click_checkout(self):
-        self.click(self.CHECKOUT_BUTTON)
+        self.wait.until(EC.url_contains("cart"))
+        self.wait.until(EC.visibility_of_element_located(self.CHECKOUT_BUTTON)).click()

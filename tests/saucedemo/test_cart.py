0ed01ad072d7a5e3aa4c8741cba328acadadce36
Fernando-Rubio <fernando.r.rubio99@gmail.com>
2026-03-12 17:07:57 -0700

    Fix cart test by navigating to cart page before assertion

--- tests/saucedemo/test_cart.py
+++ tests/saucedemo/test_cart.py
@@ -6,9 +6,8 @@ def test_add_to_cart(driver):
     login_page.open()
     login_page.login("standard_user", "secret_sauce")
 
-    inventory = InventoryPage(driver)
-    inventory.add_backpack()
-    inventory.open_cart()
+    inventory_page = InventoryPage(driver)
+    inventory_page.add_to_cart()
+    inventory_page.open_cart()
 
     assert "cart" in driver.current_url
-    

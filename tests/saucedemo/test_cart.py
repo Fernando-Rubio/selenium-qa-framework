914464f6106a4363be833aa0d351bbebf3bfdc0c
Fernando-Rubio <fernando.r.rubio99@gmail.com>
2026-03-13 18:05:28 -0700

    Added explicit wait for cart URL for CI

--- tests/saucedemo/test_cart.py
+++ tests/saucedemo/test_cart.py
@@ -1,4 +1,6 @@
 from pages.saucedemo.login_page import LoginPage
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from pages.saucedemo.inventory_page import InventoryPage
 
 def test_add_to_cart(driver):
@@ -9,5 +11,7 @@ def test_add_to_cart(driver):
     inventory_page = InventoryPage(driver)
     inventory_page.add_backpack()
     inventory_page.open_cart()
+
+    WebDriverWait(driver,15).until(EC.url_contains("cart"))
 
     assert "cart" in driver.current_url

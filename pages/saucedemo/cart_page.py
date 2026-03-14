2f9f79dcdbe16bdff2043ff38b7507ad1782aca4
Fernando-Rubio <fernando.r.rubio99@gmail.com>
2026-03-13 17:46:01 -0700

    Add explicit wait in cart_page checkout to fic CI timing issue

--- pages/saucedemo/cart_page.py
+++ pages/saucedemo/cart_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
 
@@ -6,5 +7,6 @@ class CartPage(BasePage):
     CHECKOUT_BUTTON = (By.ID, "checkout")
 
     def click_checkout(self):
+        self.wait = WebDriverWait(self.driver, 15)
         self.wait.until(EC.url_contains("cart"))
         self.wait.until(EC.visibility_of_element_located(self.CHECKOUT_BUTTON)).click()

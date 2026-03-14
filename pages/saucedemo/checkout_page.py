fef3ac0cafb105646b0a03b16cdf14f86b3af843
Fernando-Rubio <fernando.r.rubio99@gmail.com>
2026-03-14 16:16:18 -0700

    Add checkout-step-one wait for CI stability

--- pages/saucedemo/checkout_page.py
+++ pages/saucedemo/checkout_page.py
@@ -12,6 +12,7 @@ class CheckoutPage(BasePage):
     COMPLETE_HEADER = (By.CLASS_NAME, "complete-header")
 
     def enter_info(self, first, last, zip_code):
+        self.wait.until(EC.url_contains("checkout-step-one"))
         self.wait.until(EC.visibility_of_element_located(self.FIRST_NAME))
         self.type(self.FIRST_NAME,first)
         self.type(self.LAST_NAME,last)

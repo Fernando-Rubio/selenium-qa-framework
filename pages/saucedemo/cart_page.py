a0cafdb45639f1beb56eff1cbf83245cec26b688
Fernando-Rubio <fernando.r.rubio99@gmail.com>
2026-04-05 00:03:09 -0700

    Restructure of cart page for CI stability

--- pages/saucedemo/cart_page.py
+++ pages/saucedemo/cart_page.py
@@ -5,11 +5,15 @@ from pages.base_page import BasePage
 from pages.saucedemo.checkout_page import CheckoutPage
 
 class CartPage(BasePage):
+    CART_LIST = (By.CLASS_NAME, "cart_list")
     CHECKOUT_BUTTON = (By.ID, "checkout")
     REMOVE_BACKPACK = (By.ID, "remove-sauce-labs-backpack")
 
+    def open_cart(self):
+        self.wait_for_url("cart")
+        self.wait_for_element(self.CART_LIST)
+
     def click_checkout(self):
-        self.wait_for_element((By.CLASS_NAME, "cart_item"))
         self.wait_for_element(self.CHECKOUT_BUTTON).click()
     
     def remove_backpack(self):
